4d1837ad9d435de9ab37aaf7312ab9299f40229e
hyeongseop211 <gudtjq402@gmail.com>
2025-04-01 17:36:00 +0900

    250401 - Spring
    어렵다

---
+++ work_Spring/spring_13_2/src/main/java/com/lgy/spring_13_2/HomeController.java
@@ -0,0 +1,58 @@
+package com.lgy.spring_13_2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index";
+	}
+	
+	@RequestMapping("/studentView")
+//	public String StudentView(StudentInfomation studentInfomation) {
+//	클래스 이름을 stuInfo 로 임의로 바꾸면 뷰에서 값을 못 받음
+//	public String StudentView(StudentInfomation studentInfo) {
+//	@ModelAttribute : 객체 이름 변경
+	public String StudentView(@ModelAttribute("stuInfo") StudentInfomation studentInfomation) {
+		return "studentView";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
